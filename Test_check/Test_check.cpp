21bb637b4cc007a43effbe42131f261cff7995f3
1-vlad-1 <vlad01022002@mail.ru>
2022-06-13 19:46:35 +0300

    added testing function "check"

---
+++ Test_check/Test_check.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../kinpoNew/Header.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Testcheck
+{
+	TEST_CLASS(Testcheck)
+	{
+	public:
+		
+		TEST_METHOD(string_with_numbers)
+		{
+			char str[100] = " 1 2 4 5 53 ";
+
+			Assert::AreEqual(true, check(str));
+		}
+
+		TEST_METHOD(string_with_an_extra_character)
+		{
+			char str[100] = " 1 2 4 5 $ ";
+
+			Assert::AreEqual(false, check(str));
+		}
+
+		TEST_METHOD(empty_string)
+		{
+			char str[100] = "";
+
+			Assert::AreEqual(true, check(str));
+		}
+
+		TEST_METHOD(large_numbers)
+		{
+			char str[100] = "124512 51325241 ";
+
+			Assert::AreEqual(true, check(str));
+		}
+	};
+}
